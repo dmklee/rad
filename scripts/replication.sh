40c10bd49314a4a7584771d0c6a2dee59f24304c
David <klee.d@northeastern.edu>
2021-10-14 12:47:26 -0400

    fixed error with bash iteration over array

--- scripts/replication.sh
+++ scripts/replication.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-ENVS=( cartpole-swingup reacher-easy cheetah-run finger-spin walker-walk cup-catch)
+ENVS=( reacher-easy cheetah-run finger-spin walker-walk cup-catch cartpole-swingup )
 AGENTS=( rad_sac pixel_sac )
 SEEDS=( 0 1 )
 
 CUDA_ID=$1
-for seed in $SEEDS
+for seed in "${SEEDS[@]}"
 do
-	for agent in $AGENTS
+	for agent in "${AGENTS[@]}"
 	do
-		for env in $ENVS
+		for env in "${ENVS[@]}"
 		do
 			tmp=$((seed + CUDA_ID * 10))
-			CUDA_VISIBLE_DEVICES=$CUDA_ID python -m scripts.replication --seeds $tmp --envs $env --agent $agent || true
+			#echo $CUDA_ID $agent $env $tmp
+			CUDA_VISIBLE_DEVICES=$CUDA_ID python -m scripts.replication --seeds $tmp --envs $env --agent $agent
 		done
 	done
 done
